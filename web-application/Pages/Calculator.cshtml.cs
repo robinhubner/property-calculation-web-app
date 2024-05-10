66798c6a9dc85de29ecb531092ee4f88d3b64931
robin.huebner <robin.huebner@accenture.com>
2024-05-10 18:39:58 +0200

    Initial setup

---
+++ web-application/Pages/Calculator.cshtml.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace web_application.Pages;
+
+public class CalculatorModel : PageModel
+{
+    private readonly ILogger<CalculatorModel> _logger;
+
+    public CalculatorModel(ILogger<CalculatorModel> logger)
+    {
+        _logger = logger;
+    }
+
+    public void OnGet()
+    {
+    }
+}
+
